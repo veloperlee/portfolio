c53113465f419df471043b545cb6fc45eef95569
veloperlee <169134011+veloperlee@users.noreply.github.com>
2024-05-16 16:17:58 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -30,7 +30,11 @@ function random(min, max) { // min 30, max 70
 }
 
 
-
+// modal content json으로 받아오기 위해 수정
+fetch("./a.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // console.log(data.program);
 const inner = document.querySelectorAll(".inner");
 
 
@@ -57,41 +61,51 @@ function showModal(title, x, y) {
 
     }
 
+    // json으로 데이터 받아오기로 변경
+    // let content = "";
+    // if (title == "내 컴퓨터") {
+    //     content = `<article>
+    //     <div class="home_image"><img src="img/window.svg" alt="증명사진"></div>
+    //     <div class="home_text">
+    //         <h2>자기소개</h2>
+    //         <p>안녕하세요, 프론트엔드 이정우입니다. 프론트엔드 개발자로서 1년간의 경력을 보유하고 있으며 차별화된 능력과 열정으로 회사에 기여하고 싶습니다.</p>
+    //     </div>
+    //     </article>`;
+    // } else if (title == "자료실") {
+    //     content = `<h3>경력 개요</h3>
+    //     <p>
+    //     현재 니트로아이 학교사업부에서 프론트엔드 개발자로 일하고 있습니다. 이곳에서의 경험을 통해 뛰어난 HTML, CSS, JavaScript 기술을 활용하여 8개의 교육청 학교 홈페이지 개발 및 유지보수에 기여하였습니다. 특히, 교육지원청 개편 사업에서 기능 개발 협의 및 전반적인 프로젝트 총괄적인 진행을 이루어낸 경험이 있으며, 팀 내외에서의 협업 능력을 통해 프로젝트의 효율적인 진행에 기여하였습니다.</p>
+    //     <h3>기술적 강점</h3>
+    //     <p>
+    //     HTML, CSS, JavaScript에 대한 이해와 기술력을 보유하고 있습니다. 적절한 시멘틱 태그를 사용함으로써 가독성이 좋은 코드를 짜기 위해 노력하고 있습니다.</p>
+    //     <h3>역량과 성과</h3>
+    //     <p>
+    //     융화력과 책임감이라는 두 가지 큰 장점을 지녔습니다. 팀 프로젝트에서 다양한 의견을 수렴하고 통합함으로써 회의 내용을 클라이언트와 조율하며 프로젝트를 안정적으로 이루어내었습니다. 또한, 업무에 대한 책임감을 바탕으로 시켜서 하는 업무가 아닌 더 편리한 기능을 생각하며 향후 유지보수가 더 편리할 수 있도록 기존 코드를 리뉴얼하기도 하였습니다.</p>
+    //     <p>
+    //     마지막으로, 제 이력서를 읽어주셔서 감사합니다. 다양한 경험을 통해 더 나은 개발자로 성장하고, 회사의 성공에 함께 기여하고 싶습니다.
+
+    //     감사합니다.</p>`;
+    // } else if (title == "TimeLine") {
+    //     content = `<div><table><thead><td>학교명</td><td>전공</td><td>졸업상태</td></thead><tbody><tr><td>한국폴리텍대학교 아산캠퍼스</td><td>정보통신시스템 학과</td><td>졸업</td></tr></tbody></table><table><thead><td>교육기관</td><td>프로그램명</td><td>기간</td></thead><tbody><tr><td>그린컴퓨터아카데미 강남</td><td>프론트엔드 웹개발</td><td>2021.11 ~ 2021.12</td></tr><tr><td>한국소프트웨어기술진흥협회</td><td>MSA Full-Stack SW 개발자양성과정</td><td>2022.03 ~ 2022.08</td></tr></tbody></table></div>`;
+    // } else if (title == "Portfolio") {
+    //     content = `<!--<div><a href="/portfolio/page/index.html" target="_blank">페이지</a></div>-->
+    //     <div><a href="https://github.com/veloperlee/leeveloper/tree/main/team-window-theme/lee/portfolio" target="_blank">포트폴리오(현재 페이지)</a></div> 
+    //     <div><a href="https://veloperlee.github.io/todo/" target="_blank">투두리스트</a></div>
+    //      <div><a href="https://github.com/veloperlee/leeveloper/tree/main/team-window-theme/lee/brochure" target="_blank">민간 학교 브로슈어(git)</div>`;
+    // } else {
+    //     // content = "Contact 입니다.";
+    //     content = `<div>PHONE: 010-8335-2805</div>
+    //     <div>E-Mail: leevelope@naver.com</div>`
+    // }
+
     let content = "";
-    if (title == "내 컴퓨터") {
-        content = `<article>
-        <div class="home_image"><img src="img/window.svg" alt="증명사진"></div>
-        <div class="home_text">
-            <h2>자기소개</h2>
-            <p>안녕하세요, 프론트엔드 이정우입니다. 프론트엔드 개발자로서 1년간의 경력을 보유하고 있으며 차별화된 능력과 열정으로 회사에 기여하고 싶습니다.</p>
-        </div>
-        </article>`;
-    } else if (title == "자료실") {
-        content = `<h3>경력 개요</h3>
-        <p>
-        현재 니트로아이 학교사업부에서 프론트엔드 개발자로 일하고 있습니다. 이곳에서의 경험을 통해 뛰어난 HTML, CSS, JavaScript 기술을 활용하여 8개의 교육청 학교 홈페이지 개발 및 유지보수에 기여하였습니다. 특히, 교육지원청 개편 사업에서 기능 개발 협의 및 전반적인 프로젝트 총괄적인 진행을 이루어낸 경험이 있으며, 팀 내외에서의 협업 능력을 통해 프로젝트의 효율적인 진행에 기여하였습니다.</p>
-        <h3>기술적 강점</h3>
-        <p>
-        HTML, CSS, JavaScript에 대한 이해와 기술력을 보유하고 있습니다. 적절한 시멘틱 태그를 사용함으로써 가독성이 좋은 코드를 짜기 위해 노력하고 있습니다.</p>
-        <h3>역량과 성과</h3>
-        <p>
-        융화력과 책임감이라는 두 가지 큰 장점을 지녔습니다. 팀 프로젝트에서 다양한 의견을 수렴하고 통합함으로써 회의 내용을 클라이언트와 조율하며 프로젝트를 안정적으로 이루어내었습니다. 또한, 업무에 대한 책임감을 바탕으로 시켜서 하는 업무가 아닌 더 편리한 기능을 생각하며 향후 유지보수가 더 편리할 수 있도록 기존 코드를 리뉴얼하기도 하였습니다.</p>
-        <p>
-        마지막으로, 제 이력서를 읽어주셔서 감사합니다. 다양한 경험을 통해 더 나은 개발자로 성장하고, 회사의 성공에 함께 기여하고 싶습니다.
-        
-        감사합니다.</p>`;
-    } else if (title == "TimeLine") {
-        content = `<div><table><thead><td>학교명</td><td>전공</td><td>졸업상태</td></thead><tbody><tr><td>한국폴리텍대학교 아산캠퍼스</td><td>정보통신시스템 학과</td><td>졸업</td></tr></tbody></table><table><thead><td>교육기관</td><td>프로그램명</td><td>기간</td></thead><tbody><tr><td>그린컴퓨터아카데미 강남</td><td>프론트엔드 웹개발</td><td>2021.11 ~ 2021.12</td></tr><tr><td>한국소프트웨어기술진흥협회</td><td>MSA Full-Stack SW 개발자양성과정</td><td>2022.03 ~ 2022.08</td></tr></tbody></table></div>`;
-    } else if (title == "Portfolio") {
-        content = `<!--<div><a href="/portfolio/page/index.html" target="_blank">페이지</a></div>-->
-        <div><a href="https://github.com/veloperlee/leeveloper/tree/main/team-window-theme/lee/portfolio" target="_blank">포트폴리오(현재 페이지)</a></div> 
-        <div><a href="https://veloperlee.github.io/todo/" target="_blank">투두리스트</a></div>
-         <div><a href="https://github.com/veloperlee/leeveloper/tree/main/team-window-theme/lee/brochure" target="_blank">민간 학교 브로슈어(git)</div>`;
-    } else {
-        // content = "Contact 입니다.";
-        content = `<div>PHONE: 010-8335-2805</div>
-        <div>E-Mail: leevelope@naver.com</div>`
-    }
+        for (let i = 0; i < data.program.length; i++) {
+            if (data.program[i].title === title) {
+                content = data.program[i].cont;
+                break;
+            }
+        }
+
 
     const modal = document.createElement("div");
     modal.className = "modal";
@@ -177,6 +191,7 @@ function showModal(title, x, y) {
 
 }
 
+
 // 메인 ////////////////////////////////////////////////////////////////////////
 
 
@@ -184,15 +199,15 @@ function showModal(title, x, y) {
 
 // start창 모달
 const start = document.querySelector('.start');
-const startmodal = document.querySelector('.startmodal');
+const startModal = document.querySelector('.startmodal');
 start.addEventListener('click', function () {
-    startmodal.classList.toggle('hide');
+    startModal.classList.toggle('hide');
     start.classList.toggle('shadow');
 })
 
 // 바탕화면을 눌러도 start 창이 닫아지게
 document.querySelector('#main').addEventListener('click', function () {
-    startmodal.classList.add('hide');
+    startModal.classList.add('hide');
     start.classList.remove('shadow');
 })
 
@@ -237,3 +252,5 @@ for (let i = 0; i < fticon.length; i++) {
 // }
 
 
+})
+.catch(error => console.log(error));
